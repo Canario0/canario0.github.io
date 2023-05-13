5b4edc7b26903f97270f3b14554f5a894acb0968
Canario0 <prenero4bc@gmail.com>
2023-05-13 16:01:00 +0200

    feat: added experience/education sections

--- src/content.ts
+++ src/content.ts
@@ -3,7 +3,7 @@ import Home from "./pages/Home.svelte";
 import Whoami from "./pages/Whoami.svelte";
 import Experience from "./pages/Experience.svelte";
 
-export const PAGES: [Page<Home>, Page<Whoami>, Page<Experience>] = [
+export const PAGES: [Page<Home>, Page<Whoami>, Page<Experience>, Page<Experience>] = [
   {
     id: "home",
     linkName: "Home",
@@ -32,9 +32,70 @@ export const PAGES: [Page<Home>, Page<Whoami>, Page<Experience>] = [
     }
   },
   {
-    id: "#experience",
+    id: "experience",
     linkName: "Experience",
     component: Experience,
-    props: {}
+    props: {
+      sectionTitle: "Experience",
+      experienceEntries: [
+        {
+          time: "May 2022 - Now",
+          company: "Celtiberian Solutions",
+          title: "Full Stack Developer",
+          description: `
+          Working in the international market with different Typescript/JavaScript base technologies like Express, NestJs, React, Angular, and more`
+        },
+        {
+          time: "January 2021 - May 2022",
+          company: "Brain-Tec Group",
+          title: "Full Stack Developer",
+          description: `Working in the European market adapting Odoo (python base ERP) to the needs of the clients.`
+        },
+        {
+          time: "August 2020 - December 2020",
+          company: "Ayesa Spain",
+          title: "Full Stack Developer",
+          description: `Working in the Spanish Administration with Spring and Angular.`
+        },
+        {
+          time: "September 2019 - June 2020",
+          company: "University of Valladolid",
+          title: "Researcher",
+          description: `Working with different Arduino base frameworks to develop an indoor localisation system base on UWB technologies.`
+        },
+        {
+          time: "September 2018 - September 2019",
+          company: "Alten Spain",
+          title: "QA Automation",
+          description: `Testing backend and frontend, in an Adidas’ project. Using frameworks like
+Serenity, Selenium, Appium, Rest Assured.`
+        }
+      ]
+    }
+  },
+  {
+    id: "education",
+    linkName: "Education",
+    component: Experience,
+    props: {
+      sectionTitle: "Education",
+      experienceEntries: [
+        {
+          time: "2015 - 2020",
+          company:"University of Valladolid",
+          title: "Computer Engineering School of Valladolid",
+          description: `
+          <ul>
+            <li>Minor in Software Engineering.</li>
+            <li>9.9 in my dissertation.</li>
+          </ul>`
+        },
+        {
+          time: "2019",
+          company:"ISTQB",
+          title: "ISTQB Foundation Level",
+        }
+      ]
+    }
   }
 ];
--- src/pages/Experience.svelte
+++ src/pages/Experience.svelte
@@ -1,13 +1,119 @@
 <script lang="ts">
   import ArticlePage from "../lib/ArticlePage.svelte";
   import Title from "../lib/Title.svelte";
+  import arrow from "../assets/arrow.svg";
+  import Subtitle from "../lib/Subtitle.svelte";
+
+  export let sectionTitle: string;
+  export let experienceEntries: {
+    time: string;
+    company?: string;
+    title: string;
+    description?: string;
+  }[];
 </script>
 
 <ArticlePage>
   <svelte:fragment slot="title">
     <div>
-      <Title>Experience</Title>
+      <Title>{sectionTitle}</Title>
     </div>
   </svelte:fragment>
-  <div class="content" slot="content" />
+  <svelte:fragment slot="content">
+    {#each experienceEntries as { time, company, title, description } (time)}
+      <div class="experience-row">
+        <div class="experience-timeline">
+          <div class="experience-date">
+            <Subtitle>{time}</Subtitle>
+          </div>
+          <div class="timeline-divider">
+            <div class="divider" />
+            <img width="15px" height="15px" src={arrow} alt="right arrow" />
+          </div>
+          {#if company}
+            <div class="experience-company">{company}</div>
+          {/if}
+        </div>
+        <div class="experience-description">
+          <h2 class="content-title">{title}</h2>
+          {#if description}
+            <p>{@html description}</p>
+          {/if}
+        </div>
+      </div>
+    {/each}
+  </svelte:fragment>
 </ArticlePage>
+
+<style>
+  .experience-row {
+    display: flex;
+    flex-direction: row;
+    padding: 0;
+    margin: 0;
+    flex-wrap: nowrap;
+    padding-bottom: 40px;
+  }
+  .experience-row:last-child {
+    padding-bottom: 0px;
+  }
+
+  .experience-timeline {
+    width: 25%;
+    text-align: right;
+  }
+  .experience-description {
+    width: 75%;
+    padding-left: 40px;
+    padding-top: 1.5rem;
+  }
+
+  .experience-date,
+  .experience-company {
+    padding-right: 50px;
+  }
+  .experience-company {
+    color: var(--text-color-light);
+  }
+
+  .timeline-divider {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    align-items: center;
+  }
+  .divider {
+    display: inline-block;
+    border-bottom: 0;
+    border-top: 1px solid white;
+    flex-grow: 1;
+  }
+
+  .content-title {
+    font-family: "Playfair Display", serif;
+    color: var(--primary-color);
+    margin: 0;
+    font-size: 1.2rem;
+    font-weight: 500;
+    line-height: 1.2rem;
+    word-wrap: break-word;
+  }
+  /* Small Screens */
+  @media (max-width: 625px) {
+    .experience-row {
+      flex-wrap: wrap;
+    }
+
+    .experience-timeline {
+      width: 100%;
+    }
+    .experience-description {
+      width: 100%;
+      padding-left: 0px;
+    }
+    .experience-date,
+    .experience-company {
+      text-align: left;
+    }
+  }
+</style>
